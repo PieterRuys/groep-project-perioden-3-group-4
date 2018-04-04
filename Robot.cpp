052f94ad5d1ef5632568e1bede298bc53cd1f051
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-04 14:56:02 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -52,7 +52,7 @@ int main(){
       BP.set_motor_power(PORT_C, (power+((50-lightval)/2)));
     }
     if (lightval > 50){
-      BP.set_motor_power(PORT_B, (power+((ligtval - 50)/2)));
+      BP.set_motor_power(PORT_B, (power+((lightval - 50)/2)));
       BP.set_motor_power(PORT_C, (100-lightval)*power/50);
     }
     usleep(100000);
