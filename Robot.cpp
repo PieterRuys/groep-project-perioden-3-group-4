013dc06d4cf93e706df1d5973f58305df09d0b4a
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-04 13:49:51 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -42,15 +42,15 @@ int main(){
   cin >> regel;
 
   int16_t lightval;
-  int16_t power = 40;
+  int16_t power = 20;
   while(true){
     lightval = measureLight();
     if (lightval <= 50){
       BP.set_motor_power(PORT_B, lightval*power/50);
-      BP.set_motor_power(PORT_C, power);
+      BP.set_motor_power(PORT_C, power +10);
     }
     if (lightval > 50){
-      BP.set_motor_power(PORT_B, power);
+      BP.set_motor_power(PORT_B, power +10);
       BP.set_motor_power(PORT_C, (100-lightval)*power/50);
     }
     usleep(100000);
