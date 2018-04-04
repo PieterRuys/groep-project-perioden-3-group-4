c125c21c957f45faab9073bcb0f2744d21251245
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-04 12:59:06 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -13,6 +13,12 @@ uint16_t MIN;
 uint16_t MAX;
 sensor_color_t mycolor;
 
+int16_t measureLight(){
+  BP.get_sensor(PORT_1, mycolor);
+  uint16_t val = mycolor.reflected_red;
+  return (100*(val - MIN))/(MAX - MIN);
+}
+
 int main(){
   signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
 
@@ -34,3 +40,27 @@ int main(){
   cout << "MAX = " << MAX << endl;
   cout << "plaats het voertuig met de sensor half boven de lijn en voer in c gevolgd door enter" << endl;
   cin >> regel;
+
+  int16_t lightval;
+  int16_t power = 20;
+  while(true){
+    lightval = measureLight();
+    if (lightval <= 50){
+      BP.set_motor_power(PORT_B, lightval*power/50);
+      BP.set_motor_power(PORT C, power);
+    }
+    if (lightval > 50){
+      BP.set_motor_power(PORT B, power);
+      BP.set_motor_power(PORT C, 100-(lightval*power/50);
+    }
+    usleep(100000);
+  }
+ }                        
+ 
+ void exit_signal_handler(int signo){
+  if(signo == SIGINT){
+    BP.reset_all();    // Reset everything so there are no run-away motors
+    BP.set_sensor_type(PORT_1, SENSOR_TYPE_NONE);  //Doesn't work, sorry.
+    exit(-2);
+  }
+}
