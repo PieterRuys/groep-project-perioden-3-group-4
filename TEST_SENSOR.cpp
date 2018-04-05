3d3a13b93131720c9bb9a2667a75a5a3223e00a2
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-05 12:11:00 +0200

    Update TEST_SENSOR.cpp

--- TEST_SENSOR.cpp
+++ TEST_SENSOR.cpp
@@ -41,6 +41,22 @@ void dodge(sensor_ultrasonic_t Ultrasonic2){
 		BP.set_motor_position_relative(PORT_B, -550);
 		BP.set_motor_position_relative(PORT_C, 550);
 		sleep(1);
+		if(BP.get_sensor(PORT_2, Ultrasonic2) == 0){
+			if(Ultrasonic2.cm < 30){
+				BP.set_motor_position_relative(PORT_B, 550);
+				BP.set_motor_position_relative(PORT_C, -550);
+				sleep(1);
+			}
+ 		}
+		BP.set_motor_power(PORT_B, 50);
+		BP.set_motor_power(PORT_C, 50);
+		sleep(1);
+		BP.set_motor_power(PORT_B, 0);
+		BP.set_motor_power(PORT_C, 0);
+		sleep(1);
+		BP.set_motor_position_relative(PORT_B, 550);
+		BP.set_motor_position_relative(PORT_C, -550);
+		sleep(1);
 		
 	}
 }
