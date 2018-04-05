56555a6475e4df8c341cde852113d77ec85845f2
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-05 11:58:35 +0200

    Update TEST_SENSOR.cpp

--- TEST_SENSOR.cpp
+++ TEST_SENSOR.cpp
@@ -22,6 +22,7 @@ void move_aside(void){
 	sleep(1);
 	BP.set_motor_power(PORT_B, 0);
 	BP.set_motor_power(PORT_C, 0);
+	sleep(1);
 	BP.set_motor_position_relative(PORT_B, -550);
 	BP.set_motor_position_relative(PORT_C, 550);
 }
