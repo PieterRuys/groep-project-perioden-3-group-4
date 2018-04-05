bf6104057d55e1514a6e87adbcfb34745d6739c1
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-05 14:15:02 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -49,12 +49,12 @@ int main(){
     lightval = getlight();
     cout << lightval << endl;
     if (lightval <= 40){
-      BP.set_motor_power(PORT_B, -10);
-      BP.set_motor_power(PORT_C, 40);
-    }
-    if (lightval >= 60){
       BP.set_motor_power(PORT_B, (lightval*power/50)-(lightval*power/70));
       BP.set_motor_power(PORT_C, (power+10)+(lightval/50));
+    }
+    if (lightval >= 60){
+      BP.set_motor_power(PORT_B, (power+10)+((100-lightval)/50));
+      BP.set_motor_power(PORT_C, (100-lightval)*power/50)-(lightval*power/70));
     }
     if(lightval > 40 && lightval < 60){
       BP.set_motor_power(PORT_B, 30);
