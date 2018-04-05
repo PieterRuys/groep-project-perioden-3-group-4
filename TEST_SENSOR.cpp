67efa893bb055ea6486ba7629ca91ef205eee029
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-05 11:13:58 +0200

    Update TEST_SENSOR.cpp

--- TEST_SENSOR.cpp
+++ TEST_SENSOR.cpp
@@ -26,8 +26,8 @@ int main(){
 		if(Ultrasonic2.cm < 30){
 			cout << "minder" << endl;
 			
-	 		BP.set_motor_position_relative(PORT_B, 600);
-	 		BP.set_motor_position_relative(PORT_C, -600);
+	 		BP.set_motor_position_relative(PORT_B, 550);
+	 		BP.set_motor_position_relative(PORT_C, -550);
 		}
  	}
     sleep(1);
