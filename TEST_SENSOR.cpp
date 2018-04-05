74fe151db563d19ccd4b0f5c8bbb20296c33cffc
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-05 11:40:45 +0200

    Update TEST_SENSOR.cpp

--- TEST_SENSOR.cpp
+++ TEST_SENSOR.cpp
@@ -13,7 +13,7 @@ void exit_signal_handler(int signo);
 void dodge(void){
 	BP.set_motor_power(PORT_B, 0);
 	BP.set_motor_power(PORT_C, 0);
-	sleep(0.5)
+	sleep(0.5);
 	BP.set_motor_position_relative(PORT_B, 550);
 	BP.set_motor_position_relative(PORT_C, -550);
 	BP.set_motor_power(PORT_B, 20);
