37d1885428bb39deebd4588d37572fad9acd6437
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-06 10:52:45 +0200

    Update robot_sensor_2_test.cpp

--- robot_sensor_2_test.cpp
+++ robot_sensor_2_test.cpp
@@ -47,10 +47,10 @@ int main(){
   cin >> regel;
   BP.get_sensor(PORT_1, mycolor);
   BP.get_sensor(PORT_3, mylight);
-  MAX_LIGHT = mylight.reflected;
+  MIN_LIGHT = mylight.reflected;
   MIN_RGB = mycolor.reflected_blue;
   cout << "MIN_RGB = " << MIN_RGB << endl;
-  cout << "MIN_LIGHT = " << MAX_LIGHT << endl;
+  cout << "MIN_LIGHT = " << MIN_LIGHT << endl;
   cout << "plaats sensor helemaal naast de lijn (wit) en voer in b gevolgd door enter" << endl;
   cin >> regel;
   BP.get_sensor(PORT_1, mycolor);
@@ -58,7 +58,7 @@ int main(){
   MAX_RGB = mycolor.reflected_blue;
   MIN_LIGHT = mylight.reflected;
   cout << "MAX_RGB = " << MAX_RGB << endl;
-  cout << "MIN_LIGHT = " << MIN_LIGHT << endl;
+  cout << "MAX_LIGHT = " << MAX_LIGHT << endl;
   cout << "plaats het voertuig met de sensor half boven de lijn en voer in c gevolgd door enter" << endl;
   cin >> regel;
 
