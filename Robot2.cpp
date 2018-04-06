ae047e5c645beee7781c74c49fff2bdd7dd35bf7
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-06 12:26:30 +0200

    Update Robot2.cpp

--- Robot2.cpp
+++ Robot2.cpp
@@ -68,7 +68,7 @@ int main(){
     cout << lightval << endl;
     cout << colorval << endl;
     if (lightval > 30){
-      BP.set_motor_power(PORT_B, (lightval-30));
+      BP.set_motor_power(PORT_B, (lightval-50));
       BP.set_motor_power(PORT_C, power-((lightval-30)/1.5));
     }
     if (colorval < 50){
