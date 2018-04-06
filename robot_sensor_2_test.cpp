64ecd60954a40111c1f872fd55db6a88d3222d65
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-06 10:14:55 +0200

    Update robot_sensor_2_test.cpp

--- robot_sensor_2_test.cpp
+++ robot_sensor_2_test.cpp
@@ -27,13 +27,15 @@ int main(){
   BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
 
   BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_RED);
+  BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
 
   //calibrate
   string regel;
   cout << "plaats sensor recht boven de lijn (zwart) en voer in a gevolgd door enter" << endl;
   cin >> regel;
   BP.get_sensor(PORT_1, mycolor);
-  MIN = mycolor.reflected_red;
+  BP.get_sensor(PORT_3, mylight);
+  MIN_ = mycolor.reflected_red;
   cout << "MIN = " << MIN << endl;
   cout << "plaats sensor helemaal naast de lijn (wit) en voer in b gevolgd door enter" << endl;
   cin >> regel;
