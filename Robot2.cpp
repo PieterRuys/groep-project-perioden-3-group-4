eb642b4dc61b1cebc284738328eae36716eea94d
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-09 11:04:11 +0200

    Update Robot2.cpp

--- Robot2.cpp
+++ Robot2.cpp
@@ -143,6 +143,12 @@ int main(){
       BP.set_motor_power(PORT_B, power);
       BP.set_motor_power(PORT_C, power);
     }
+    if(colorval < 40 && lightval > 60){
+      	BP.set_motor_power(PORT_B, 0);
+	BP.set_motor_power(PORT_C, 0);
+	BP.set_motor_position_relative(PORT_B, -250);
+	BP.set_motor_position_relative(PORT_C, 250);
+	sleep(1);
     usleep(100000);
  }   
   
