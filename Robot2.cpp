b0015ed9e1c630c901a7647bb7edfe8b1186b4e4
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-09 10:37:37 +0200

    Update Robot2.cpp

--- Robot2.cpp
+++ Robot2.cpp
@@ -71,16 +71,19 @@ void dodge(sensor_ultrasonic_t Ultrasonic2){
 				turn_right();
 			}
 			else{
-				//hier forward tot de sensoren een lijn zien dan turn_right(); en  dan verder gaan met het nromale protocol
-				BP.set_motor_power(PORT_B, 20);
-				BP.set_motor_power(PORT_C, 20);
-				if(getcolor() < 40){
-					cout << "check" << endl;
+				while(true){
+					//hier forward tot de sensoren een lijn zien dan turn_right(); en  dan verder gaan met het nromale protocol
 					BP.set_motor_power(PORT_B, 20);
 					BP.set_motor_power(PORT_C, 20);
-					turn_right();
-					done ++;
+					if(getcolor() < 40){
+						cout << "check" << endl;
+						BP.set_motor_power(PORT_B, 20);
+						BP.set_motor_power(PORT_C, 20);
+						turn_right();
+						break;
+					}
 				}
+				done++
 			}
  		}
 	}
