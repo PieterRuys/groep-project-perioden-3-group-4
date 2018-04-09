a93e00ac024013dac8c51b6e4683b06535548d68
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-09 10:06:16 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -9,16 +9,27 @@ BrickPi3 BP;
 
 void exit_signal_handler(int signo);
 
-uint16_t MIN;
-uint16_t MAX;
+uint16_t MinLight;
+uint16_t MinColor;
+uint16_t MaxLight;
+uint16_t MaxColor;
+sensor_light_t mylight;
 sensor_color_t mycolor;
 
 int16_t getlight(){
+  BP.get_sensor(PORT_3, mylight);
+  int16_t val = mylight.reflected;
+  if (val < MinLight) val = MinLight;
+  if (val > MaxLight) val = MaxLight;
+  return (100*(val - MinLight))/(MaxLight - MinLight);
+}
+
+int16_t getcolor(){
   BP.get_sensor(PORT_1, mycolor);
   uint16_t val = mycolor.reflected_red;
-  if (val < MIN) val = MIN;
-  if (val > MAX) val = MAX;
-  return (100*(val - MIN))/(MAX - MIN);
+  if (val < MinColor) val = MinColor;
+  if (val > MaxColor) val = MaxColor;
+  return (100*(val - MinColor))/(MaxColor - MinColor);
 }
 
 int main(){
@@ -26,40 +37,54 @@ int main(){
 
   BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
 
+  BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
   BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_RED);
-
-  //calibrate
+  
   string regel;
   cout << "plaats sensor recht boven de lijn (zwart) en voer in a gevolgd door enter" << endl;
   cin >> regel;
+  BP.get_sensor(PORT_3, mylight);
   BP.get_sensor(PORT_1, mycolor);
-  MIN = mycolor.reflected_red;
-  cout << "MIN = " << MIN << endl;
-  cout << "plaats sensor helemaal naast de lijn (wit) en voer in b gevolgd door enter" << endl;
+  MaxLight = mylight.reflected;
+  MinColor = mycolor.reflected_red;
+  cout << "MaxLight =" << MaxLight << endl;
+  cout << "MinColor =" << MinColor << endl;
+  cout << "plaats sensor recht boven de lijn (wit) en voer in a gevolgd door enter" << endl;
   cin >> regel;
+  BP.get_sensor(PORT_3, mylight);
   BP.get_sensor(PORT_1, mycolor);
-  MAX = mycolor.reflected_red;
-  cout << "MAX = " << MAX << endl;
-  cout << "plaats het voertuig met de sensor half boven de lijn en voer in c gevolgd door enter" << endl;
-  cin >> regel;
-
+  MinLight = mylight.reflected;
+  MaxColor = mycolor.reflected_red;
+  cout << "MinLight =" << MinLight << endl;
+  cout << "MaxColor =" << MaxColor << endl;
+  
   int16_t lightval;
+  int16_t colorval;
   int16_t power = 20;
+  
   while(true){
     lightval = getlight();
+    colorval = getcolor();
     cout << lightval << endl;
-    if (lightval <= 50){
-      BP.set_motor_power(PORT_B, (lightval*power/50)-(lightval*power/70));
-      BP.set_motor_power(PORT_C, (power+10)+(lightval/50));
+    cout << colorval << endl;
+    if (lightval > 30){
+      BP.set_motor_power(PORT_B, power+((lightval-30)/1.5));
+      BP.set_motor_power(PORT_C, power-((lightval-30)/1.5));
     }
-    if (lightval >= 50){
-      BP.set_motor_power(PORT_B, (power+10)+((100-lightval)/50));
-      BP.set_motor_power(PORT_C, ((100-lightval)*power/50)-(lightval*power/70));
+    if (colorval < 70){
+      BP.set_motor_power(PORT_B, power-((70-lightval)/1.5));
+      BP.set_motor_power(PORT_C, power+((70- lightval)/1.5));
+    }
+    if(lightval < 30 && colorval > 50){
+      BP.set_motor_power(PORT_B, power);
+      BP.set_motor_power(PORT_C, power);
     }
     usleep(100000);
- } 
- }                        
- 
+ }   
+  
+}
+  
+  
  void exit_signal_handler(int signo){
   if(signo == SIGINT){
     BP.reset_all();    // Reset everything so there are no run-away motors
