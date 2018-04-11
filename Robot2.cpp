71bc5d239f071bedd962f9f079ad54cb944bf983
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-11 14:23:38 +0200

    Update Robot2.cpp

--- Robot2.cpp
+++ Robot2.cpp
@@ -130,15 +130,15 @@ int main(){
 			    dodge(Ultrasonic2);
 		    }
  	    }
-    if (lightval > 40){
+    if (lightval < 60){
       BP.set_motor_power(PORT_B, 20+((lightval-40)/1.5));
       BP.set_motor_power(PORT_C, 20-((lightval-40)/1.5));
     }
-    else if (colorval < 60){
+    else if (colorval > 60){
       BP.set_motor_power(PORT_B, 20-((80-colorval)/1.5));
       BP.set_motor_power(PORT_C, 20+((80-colorval)/1.5));
     }
-    else if(lightval < 40 && colorval > 60){
+    else if(lightval > 60 && colorval > 60){
       BP.set_motor_power(PORT_B, 20);
       BP.set_motor_power(PORT_C, 20);
     }
