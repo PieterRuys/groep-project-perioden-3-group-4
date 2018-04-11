f77b9b6c03cbc384115d926ceb556f5bbaf9fcac
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-11 17:28:47 +0200

    Update Robot2.cpp

--- Robot2.cpp
+++ Robot2.cpp
@@ -129,15 +129,15 @@ int main(){
  	    }
     if (lightval <= 70){
       BP.set_motor_power(PORT_B, (20+(60-lightval)/1.5));
-      BP.set_motor_power(PORT_C, (20-(80-lightval)/1.5));
+      BP.set_motor_power(PORT_C, (30-(80-lightval)/1.5));
     }
     else if (colorval <= 70){
-      BP.set_motor_power(PORT_B, (20-(80-colorval)/1.5));
+      BP.set_motor_power(PORT_B, (30-(80-colorval)/1.5));
       BP.set_motor_power(PORT_C, (20+(70-colorval)/1.5));
     }
     else if(lightval > 40 && colorval > 40){
-      BP.set_motor_power(PORT_B, 20);
-      BP.set_motor_power(PORT_C, 20);
+      BP.set_motor_power(PORT_B, 30);
+      BP.set_motor_power(PORT_C, 30);
     }
     usleep(100000);
  }   
