ca44b695c50a8166fde4f66cdf3d466bdff3706a
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-11 14:27:18 +0200

    Update Robot2.cpp

--- Robot2.cpp
+++ Robot2.cpp
@@ -131,8 +131,8 @@ int main(){
 		    }
  	    }
     if (lightval <= 60){
-      BP.set_motor_power(PORT_B, 20+((lightval-40)/1.5));
-      BP.set_motor_power(PORT_C, 20-((lightval-40)/1.5));
+      BP.set_motor_power(PORT_B, 20+((80-lightval)/1.5));
+      BP.set_motor_power(PORT_C, 20-((80-lightval)/1.5));
     }
     else if (colorval <= 60){
       BP.set_motor_power(PORT_B, 20-((80-colorval)/1.5));
