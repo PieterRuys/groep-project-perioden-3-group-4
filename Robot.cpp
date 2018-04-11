09df951f16ce40c178739e554d4d537b1b17e89a
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-11 14:29:19 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -21,7 +21,7 @@ int16_t getlight(){
   int16_t val = mylight.reflected;
   if (val < MinLight) val = MinLight;
   if (val > MaxLight) val = MaxLight;
-  return (100*(val - MinLight))/(MaxLight - MinLight);
+  return 100-((100*(val - MinLight))/(MaxLight - MinLight));
 }
 
 int16_t getcolor(){
@@ -32,13 +32,72 @@ int16_t getcolor(){
   return (100*(val - MinColor))/(MaxColor - MinColor);
 }
 
+void turn_left(void){
+	BP.set_motor_power(PORT_B, 0);
+	BP.set_motor_power(PORT_C, 0);
+	BP.set_motor_position_relative(PORT_B, -500);
+	BP.set_motor_position_relative(PORT_C, 500);
+	sleep(1);
+}
+
+void turn_right(void){
+	BP.set_motor_power(PORT_B, 0);
+	BP.set_motor_power(PORT_C, 0);
+	BP.set_motor_position_relative(PORT_B, 500);
+	BP.set_motor_position_relative(PORT_C, -500);
+	sleep(1);	
+}
+
+void forward(int time){
+	BP.set_motor_power(PORT_B, 30);
+	BP.set_motor_power(PORT_C, 30);
+	sleep(time);
+}
+
+void move_aside(void){
+	turn_right();
+	forward(2);
+	turn_left();
+}
+
+void dodge(sensor_ultrasonic_t Ultrasonic2){
+	int done = 0;
+	move_aside();
+	while(done == 0){
+		forward(1);
+		turn_left();
+		if(BP.get_sensor(PORT_2, Ultrasonic2) == 0){
+			if(Ultrasonic2.cm < 30){
+				turn_right();
+			}
+			else{
+				BP.set_motor_power(PORT_B, 30);
+				BP.set_motor_power(PORT_C, 30);
+				while(true){
+					if(getlight() > 60){
+						BP.set_motor_power(PORT_B, 40);
+						BP.set_motor_power(PORT_C, 40);
+						sleep(1);
+						turn_right();
+						break;
+					}
+				}
+			done++;
+			}
+ 		}
+	}
+}
+
 int main(){
   signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
 
   BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
 
   BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
+  BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_ULTRASONIC);
   BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_RED);
+  
+  sensor_ultrasonic_t Ultrasonic2;
   
   string regel;
   cout << "plaats sensor recht boven de lijn (zwart) en voer in a gevolgd door enter" << endl;
@@ -60,24 +119,28 @@ int main(){
   
   int16_t lightval;
   int16_t colorval;
-  int16_t power = 10;
   
   while(true){
     lightval = getlight();
     colorval = getcolor();
     cout << lightval << endl;
     cout << colorval << endl;
-    if (lightval > 40){
-      BP.set_motor_power(PORT_B, power+((lightval-20)/1.5));
-      BP.set_motor_power(PORT_C, -((lightval-40)/1.5));
+    if(BP.get_sensor(PORT_2, Ultrasonic2) == 0){
+		    if(Ultrasonic2.cm < 15){
+			    dodge(Ultrasonic2);
+		    }
+ 	    }
+    if (lightval <= 60){
+      BP.set_motor_power(PORT_B, 20+((80-lightval)/1.5));
+      BP.set_motor_power(PORT_C, 20-((80-lightval)/1.5));
     }
-    else if (colorval < 60){
-      BP.set_motor_power(PORT_B, -((80-colorval)/1.5));
-      BP.set_motor_power(PORT_C, power+((80-colorval)/1.5));
+    else if (colorval <= 60){
+      BP.set_motor_power(PORT_B, 20-((80-colorval)/1.5));
+      BP.set_motor_power(PORT_C, 20+((80-colorval)/1.5));
     }
-    else if(lightval < 40 && colorval > 60){
-      BP.set_motor_power(PORT_B, power+5);
-      BP.set_motor_power(PORT_C, power+5);
+    else if(lightval > 60 && colorval > 60){
+      BP.set_motor_power(PORT_B, 20);
+      BP.set_motor_power(PORT_C, 20);
     }
     usleep(100000);
  }   
