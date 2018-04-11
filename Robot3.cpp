f96dd0419e137edc8d12515c162d311c5d88bddb
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-04-11 11:39:06 +0200

    Update Robot3.cpp

--- Robot3.cpp
+++ Robot3.cpp
@@ -68,8 +68,8 @@ int main(){
     cout << lightval << endl;
     cout << colorval << endl;
     if (colorval >= 60){
-      BP.set_motor_power(PORT_B, power+((colorval-60)/1.5));
-      BP.set_motor_power(PORT_C, -((colorval-60)/1.5));
+      BP.set_motor_power(PORT_B, power+((colorval-80)/1.5));
+      BP.set_motor_power(PORT_C, -((colorval-80)/1.5));
     }
     else if (colorval <= 40){
       BP.set_motor_power(PORT_B, -((40-colorval)/1.5));
