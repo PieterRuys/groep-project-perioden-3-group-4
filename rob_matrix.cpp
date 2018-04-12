3be0bda6c6e3f3348a3c3fa592cbcbfc5bf0cdf4
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-12 16:32:07 +0200

    Update rob_matrix.cpp

--- rob_matrix.cpp
+++ rob_matrix.cpp
@@ -138,9 +138,14 @@ bool pos_move_one_step(struct Pos &pos, int direction) {
 bool detect_obstacle_ahead(struct Pos rob_pos, int direction, sensor_ultrasonic_t Ultrasonic2){
 	if ( pos_move_one_step(rob_pos, direction) ) {
 		cout << "hoi" << endl;
+	BP.set_motor_power(PORT_B, 0);
+        BP.set_motor_power(PORT_C, 0);
 	if(next_crosing_free(Ultrasonic2)) board[rob_pos.x][rob_pos.y] = 'X';
 	//if ( rob_pos.x == 2 && rob_pos.y == 0 ) board[rob_pos.x][rob_pos.y] = 'X';//temporary obstacel
 	}
+	sleep(1);
+	BP.set_motor_power(PORT_B, 30);
+        BP.set_motor_power(PORT_C, 30);
 }
 
 bool run(struct Pos rob_pos, int direction, sensor_ultrasonic_t Ultrasonic2) {
