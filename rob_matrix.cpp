31ad36289f52753661236c2e0dde6d9b69e3359b
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-12 16:45:53 +0200

    Update rob_matrix.cpp

--- rob_matrix.cpp
+++ rob_matrix.cpp
@@ -63,7 +63,6 @@ void robot_turn_right(void){
 
 bool next_crosing_free(sensor_ultrasonic_t Ultrasonic2){
  	if(BP.get_sensor(PORT_2, Ultrasonic2) == 0){
-		cout << Ultrasonic2.cm << endl;
 		if(Ultrasonic2.cm < 20){
 			cout << "object" << endl;
 			return true;
