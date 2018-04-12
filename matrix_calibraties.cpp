f958d42c9fbeaab0acda5e8ef20fb0dbfbab2ebc
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-12 08:28:44 +0200

    Update matrix_calibraties.cpp

--- matrix_calibraties.cpp
+++ matrix_calibraties.cpp
@@ -80,8 +80,8 @@ cout << lightval << endl;
     }
    
     else{
-      BP.set_motor_power(PORT_B, 25);
-      BP.set_motor_power(PORT_C, 25);
+      BP.set_motor_power(PORT_B, 30);
+      BP.set_motor_power(PORT_C, 30);
     }
     usleep(100000);
   }
