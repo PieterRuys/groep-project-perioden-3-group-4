7fe0970a1ff950478bf18a10104861c6b6e104b0
PieterRuys <33416185+PieterRuys@users.noreply.github.com>
2018-04-12 16:13:15 +0200

    Update rob_matrix.cpp

--- rob_matrix.cpp
+++ rob_matrix.cpp
@@ -77,7 +77,7 @@ void robot_forward_one_step(){
   while(true){
     int16_t colorval = getcolor();
     int16_t lightval = getlight();
-    cout << lightval << endl;
+    //cout << lightval << endl;
     
     if(lightval <= 20 && colorval <= 20){
       //drive until you are on the crossing
